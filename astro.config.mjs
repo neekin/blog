01075c9b0279d6133ef85add6a79d686657c0092
Nee Kin <njgzs360@gmail.com>
2023-10-21 03:39:19 +0800

    update settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,5 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-// import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://neekin.github.io',
-  base: '/blog',
-  integrations: [mdx(), sitemap()],
-  // output: "server",
-  // adapter: vercel()
+  site: 'https://blog.yes33.cn'
 });
