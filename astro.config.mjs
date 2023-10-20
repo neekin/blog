b4d30b195116df1d6f7b556455d0296861863814
Nee Kin <njgzs360@gmail.com>
2023-10-21 03:32:51 +0800

    deploy github

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://neekin.github.io',
   base: '/blog',
   integrations: [mdx(), sitemap()],
-  output: "server",
-  adapter: vercel()
+  // output: "server",
+  // adapter: vercel()
 });
