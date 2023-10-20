dcc6ef49c2ce859f9758dc4b2b64d469ac2d2a7b
Nee Kin <njgzs360@gmail.com>
2023-10-21 03:27:17 +0800

    add github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://neekin.github.io',
+  base: '/blog',
   integrations: [mdx(), sitemap()],
   output: "server",
   adapter: vercel()
